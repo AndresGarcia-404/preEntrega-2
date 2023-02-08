980b9846ebcf9e28a3fbb19f350a18232f406d33
Andres Garcia <A01800027@tec.mx>
2023-02-08 18:21:46 -0500

    f products

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -59,6 +59,45 @@ router.get('/', async (req,res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const result = await productModel.findById(id);
+        res.status(200).send(result);
+    } catch (error) {
+        res.status(404).send("product not found")
+    }
+})
 
+router.post('/',async (req, res) => {
+    const {title,description,code,price,status,stock,category,thumbnail} = req.body;
+    const newProduct = {title,description,code,price,status,stock,category,thumbnail}
+    try {
+        const result = await productModel.create(newProduct);
+        res.status(200).send({status:"succes",payload:result})
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const result = await productModel.findByIdAndUpdate(id,req.body)
+        res.status(200).send({status:"success",payload:result})
+    } catch (error) {
+        res.status(404).send("Product not found")
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        await productModel.findByIdAndDelete(id)
+        res.status(200).send("Product deleted")
+    } catch (error) {
+        res.status(404).send("Product not found")
+    }
+})
 
 export default router;
