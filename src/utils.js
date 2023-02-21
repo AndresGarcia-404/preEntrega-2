3b8922d50bca7298b77ba2e58a3f15c65297783b
Andres Garcia <A01800027@tec.mx>
2023-02-21 00:58:56 -0600

    limpieza

--- src/utils.js
+++ src/utils.js
@@ -6,10 +6,8 @@ export const createHash = (password) =>
   bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
 export const isValidPassword = (password, user) => {
-  //console.log({ plainText: password, hash: user.password });
   return bcrypt.compareSync(password, user.password);
 };
-// bcrypt.compareSync(createHash(password), user.password);
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
